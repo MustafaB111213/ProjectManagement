671cb04466463141eecca1d7e821785d6a8dd11d
Mustafa BAL <mbal111213@gmail.com>
2025-11-11 09:05:06 +0300

    .

--- ProjectManagement/ProjectManagementUI/src/data/users.ts
+++
@@ -1,8 +0,0 @@
-import type { User } from '../types';
-
-export const DUMMY_USERS: User[] = [
-    { id: 1, name: 'Mustafa', initials: 'M', avatarUrl: 'https://cdn1.monday.com/dapulse_default_photo.png' },
-    { id: 2, name: 'Ayşe Yılmaz', initials: 'AY' },
-    { id: 3, name: 'John Doe', initials: 'JD', avatarUrl: 'https://randomuser.me/api/portraits/men/32.jpg' },
-    { id: 4, name: 'Jane Smith', initials: 'JS', avatarUrl: 'https://randomuser.me/api/portraits/women/44.jpg' },
-];
